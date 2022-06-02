b721aa3fce5f1bd4fee3701f9954122af60cb8e2
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2022-06-02 22:11:40 +0700

    Response Util

---
+++ src/main/java/com/backend/vaccinebookingsystem/domain/common/ApiResponseStatus.java
@@ -0,0 +1,22 @@
+package com.backend.vaccinebookingsystem.domain.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ApiResponseStatus {
+
+    private String code;
+
+    private String message;
+}
