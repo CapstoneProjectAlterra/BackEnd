4f3d4ea99b22fcac15b0bca0ab63d31b2667a855
dim-sen <dimas.10119285@mahasiswa.unikom.ac.id>
2022-06-28 18:14:05 +0700

    Bugs Fix

--- src/main/java/com/backend/vaccinebookingsystem/service/AuthenticationService.java
+++ src/main/java/com/backend/vaccinebookingsystem/service/AuthenticationService.java
@@ -19,7 +19,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
